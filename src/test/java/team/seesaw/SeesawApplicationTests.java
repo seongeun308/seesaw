303239801a26badea41c97f92638090d5b01cbd3
seongeun <kse011010@gmail.com>
2023-07-24 17:15:44 +0900

    inital setting

---
+++ src/test/java/team/seesaw/SeesawApplicationTests.java
@@ -0,0 +1,13 @@
+package team.seesaw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeesawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
